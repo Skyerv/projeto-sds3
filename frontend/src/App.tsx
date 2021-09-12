e044bf49139d68f98dab063f0daef7427cc613e3
Amanda Rodrigues <amanda@manzoti.com>
2021-09-12 15:40:46 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import Navbar from "components/navbar";
-import BarChart from "components/navbar/BarChart";
-import DataTable from "components/navbar/DataTable";
-import DonutChart from "components/navbar/DonutChart";
-import Footer from "components/navbar/Footer";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <Navbar />
-        <h1 className="text-primary py-3">Dashboard de vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className = "text-center text-secondary">Vendas</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5>Taxa de sucesso de vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className = "py-3">
-        <h2 className = "text-primary">Vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+      <Routes />
   );
 }
 
